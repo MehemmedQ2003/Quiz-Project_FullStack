46898eca8995fc6e098b2ebdef6d5f425e37dae1
MetinQardasov11 <metin.qardasov2003@gmail.com>
2024-11-02 15:27:09 +0400

    Frontend and backend configuration added

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import *
 from .serializers import *
+from rest_framework.response import Response
 
-class SurahViewSet(viewsets.ModelViewSet):
+class SurahViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Surah.objects.all()
     serializer_class = SurahSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        surah = self.get_object()
+        ayahs = Ayah.objects.filter(surah=surah)
+        ayah_serializer = AyahSerializer(ayahs, many=True)
+        surah_data = SurahSerializer(surah).data
+        surah_data['ayahs'] = ayah_serializer.data
+        return Response(surah_data)
     
 class AyahViewSet(viewsets.ModelViewSet):
     queryset = Ayah.objects.all()
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,11 +40,7 @@ INSTALLED_APPS = [
     
     # third party apps
     'rest_framework',
-    # 'corsheaders',
-    # 'dj_rest_auth',
-    # 'allauth',
-    # 'allauth.account',
-    # 'allauth.socialaccount',
+    'corsheaders',
     
     # my apps
     'api.apps.ApiConfig',
@@ -54,11 +50,16 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
 ]
 
 ROOT_URLCONF = 'backend.urls'
